a8ff8058f5737b90aadaf1446b4cd72a55e754ee
rachavalrovira <ramiro.achaval@nubiral.com>
2024-09-21 11:51:10 -0300

    test

--- resources/main.tf
+++ resources/main.tf
@@ -5,7 +5,7 @@ data "terraform_remote_state" "iam" {
   backend = "s3"
   config = {
     bucket = "iam-demo-bucket-876278403405"
-    key    = "demoiam/iam-roles/${terraform.workspace}/terraform.tfstate"
+    key    = "demoiam/iam-roles/dev/terraform.tfstate"
     region = var.aws_region
   }
 }
